f861c75f931db50f4e282e085a46152ef2ca3618
Hilit Oreny <HilitOreny@users.noreply.github.com>
2020-10-17 08:52:33 +0300

    changing parameters names and splitting functions

--- hangman.py
+++ hangman.py
@@ -11,21 +11,21 @@ def create_word_list():
     return word_list
 
 
-def choose_word(new_list: list):
-    random_word = random.choice(new_list)
+def choose_word(words_from_file_list: list):
+    random_word = random.choice(words_from_file_list)
     return random_word
 
 
-def create_underscore(secret_word):
+def create_underscore(word1):
     beginning_string: str = ""
-    for _ in secret_word:
+    for _ in word1:
         beginning_string = beginning_string + "_ "
     return beginning_string
 
 
-def reveal_progress(secret_word, previous_guesses):
+def reveal_progress(word2, previous_guesses):
     user_interface = ""
-    for letter in secret_word:
+    for letter in word2:
         if letter in previous_guesses:
             user_interface = user_interface + letter
         else:
@@ -46,7 +46,7 @@ def format_guesses_alphabetical_order(previous_guesses):
     return alphabetical_final
 
 
-def present_guesses(previous_guesses, lower_cased_guessed):
+def present_guesses(previous_guesses, one_letter):
     if len(previous_guesses) > 2:
         print("Here is a list of the letters you have guessed by guessing order: ")
         current_list = format_guesses_user_order(previous_guesses)
@@ -61,36 +61,48 @@ def present_guesses(previous_guesses, lower_cased_guessed):
         alphabetical_two_letters = sorted(previous_guesses)
         print(alphabetical_two_letters[0], "and", alphabetical_two_letters[1])
     else:
-        print("You have only guessed one letter:", lower_cased_guessed)
+        print("You have only guessed one letter:", one_letter)
 
 
-def input_validation(lower_cased_guessed):
-    compare = lower_cased_guessed not in previous_guesses
-    only_english = lower_cased_guessed in "abcdefghijklmnopqrstuvwxyz"
-    valid = only_english and len(lower_cased_guessed) == 1 and compare
+def input_validation(character):
+    compare = character not in previous_guesses
+    only_english = character in "abcdefghijklmnopqrstuvwxyz"
+    valid = only_english and len(character) == 1 and compare
     if valid:
-        previous_guesses.append(lower_cased_guessed)
-    if len(lower_cased_guessed) > 1:
+        previous_guesses.append(character)
+    if len(character) > 1:
         print("Please suggest just one letter...")
     if not only_english:
         print("""Please suggest a letter of English alphabet.
               No numbers, symbols or any other letters, please...""")
     if not compare:
         print("You have already guessed this letter...Please suggest a different letter.")
-        present_guesses(previous_guesses, lower_cased_guessed)
+        present_guesses(previous_guesses, character)
     return valid
 
 
-def counting_mistakes(secret_word, lower_cased_guessed):
-    mistakes_list = []
-    if lower_cased_guessed not in secret_word:
-        mistakes_list.append("mistake")
-        print("Wrong guess! A poor man will be soon hanged!")
-    else:
+def check_letter(word3, character2):
+    check_in = character2 in word3
+    return check_in
+
+
+def checking_message(true):
+    if true:
         print("Correct guess!")
-    mistakes_list_num = len(mistakes_list)
-    print("Number of mistakes: ", mistakes_list_num)
-    return mistakes_list_num
+    else:
+        print("Wrong guess! A poor man will be soon hanged!")
+
+
+def create_mistakes_list(true_guess, character3, user_mistakes_list):
+    if not true_guess:
+        user_mistakes_list.append(character3)
+    return user_mistakes_list
+
+
+def counting_mistakes(my_list):
+    my_list_num = len(my_list)
+    print("Number of mistakes: ", my_list_num)
+    return my_list_num
 
 
 def diagram(number):
@@ -136,11 +148,11 @@ def diagram(number):
     return current_diagram
 
 
-def check_winning(secret_word, previous_guesses):
+def check_winning(word4, previous_guesses):
     checking_list = []
-    for letter in secret_word:
-        check_letter = str(letter in previous_guesses)
-        checking_list.append(check_letter)
+    for letter in word4:
+        check_this_letter = str(letter in previous_guesses)
+        checking_list.append(check_this_letter)
     you_won = "False" not in checking_list
     if you_won:
         print("Congratulations, you won!")
@@ -148,6 +160,7 @@ def check_winning(secret_word, previous_guesses):
 
 
 def game_loop():
+    mistakes_list = []
     mistakes_num = 0
     while mistakes_num < 6:
         letter_guessed: str = input("Please suggest a letter: ")
@@ -158,8 +171,11 @@ def game_loop():
         progress = reveal_progress(secret_word, previous_guesses)
         print(progress)
         present_guesses(previous_guesses, lower_cased_guessed)
-        mistakes_num = counting_mistakes(secret_word, lower_cased_guessed)
-        diagram(mistakes_num)
+        correct_guess = check_letter(secret_word, lower_cased_guessed)
+        checking_message(correct_guess)
+        your_mistakes_list = create_mistakes_list(correct_guess, lower_cased_guessed, mistakes_list)
+        mistakes_num = counting_mistakes(your_mistakes_list)
+        print(diagram(mistakes_num))
         if mistakes_num == 6:
             print("Game over! You lost!")
             print("Your secret word was:", secret_word)
